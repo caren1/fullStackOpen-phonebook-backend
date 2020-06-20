faf77f906ac91ca234195f88db9c90babfd9db3a
caren1 <czarnocki.w@wp.pl>
2020-06-20 20:50:12 +0200

    3.20* - 3.21 phonebook database, step 8 and deplyoing the database backend to production

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+
 const Person = require('./models/person')
 
 app.use(cors())
